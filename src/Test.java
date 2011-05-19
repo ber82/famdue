fbca33ba26f10761fa0359ab6ac36556361ca7bf
ber <ber@192.168.1.2>
2011-05-19 22:11:30 +0200

    Signed-off-by: ber <ber@192.168.1.2>

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 
+
 public class Test {
 	public static void main(String args[]){
 		System.out.println("Hello git 2");
+		new gui();
 	}
 }
--- src/gui.java
+++ src/gui.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class gui {
 	public gui(){
 		JFrame myFrame = new JFrame("GUI");
+		myFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		myFrame.setSize(100,100);
 		myFrame.setVisible(true);
 	}	
 }
