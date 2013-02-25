0cdd9ec03534b8c7c6627c35e0859a295fef6c71
ber82-work <monsterber@gmx.de>
2013-02-25 10:39:20 +0100

    test, commit @ work

--- src/gui.java
+++ src/gui.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class gui {
 	public gui(){
+		// edit 25.02.2013 - check at home!!!
 		JFrame myFrame = new JFrame("GUI");
 		myFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		myFrame.setSize(100,100);
