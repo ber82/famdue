e5ad2775b45262985fa58adf0295afc0f404ce2d
ber82-work <rilka@CSE2548N.claas.local>
2013-02-26 08:08:31 +0100

    test2

--- src/gui.java
+++ src/gui.java
@@ -5,7 +5,8 @@ public class gui {
 		// edit 25.02.2013 - check at home!!!
 		JFrame myFrame = new JFrame("GUI");
 		myFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		myFrame.setSize(100,100);
+		myFrame.setSize(200,100);
 		myFrame.setVisible(true);
+		System.out.println("Test");
 	}	
 }
