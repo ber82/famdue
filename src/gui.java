b602ec7ba8af5d6749dc4b268e70603bd6363bf2
ber82-work <rilka@CSE2548N.claas.local>
2013-02-27 16:13:04 +0100

    animation

--- src/gui.java
+++ src/gui.java
@@ -1,12 +1,33 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class gui {
+
+	JLabel text;
+	
 	public gui(){
 		// edit 25.02.2013 - check at home!!!
 		JFrame myFrame = new JFrame("GUI");
 		myFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		myFrame.setSize(200,100);
+		
+		myFrame.setLayout(null);
+		text = new JLabel("Test");
+		text.setOpaque(true);
+		text.setBackground(new Color(220,220,200));
+		text.setBounds(10,10,40,20);
+		myFrame.add(text);
+		myFrame.setSize(320,180);
 		myFrame.setVisible(true);
 		System.out.println("Test 2");
-	}	
+	}
+	
+	public void counter(){
+		
+	}
+	
+	public void setPos(int x, int y){
+		text.setBounds(x,y,40,20);
+	}
+	
 }
