e1b02f83f4286917c3db343125fe464f9c63a5dd
ber82-work <rilka@CSE2548N.claas.local>
2013-02-27 16:13:38 +0100

    new class

---
+++ src/Task.java
@@ -0,0 +1,78 @@
+
+
+import java.util.TimerTask;
+
+class Task extends TimerTask
+{
+	
+	gui mygui;
+	static int x = 10;
+	static int y = 10;
+	static int xDir = 0;
+	static int yDir = 0;
+	
+	Task(gui mygui){
+		this.mygui = mygui;
+	}
+  @Override public void run()
+  {
+   // System.out.println( "Make my day." );
+    mygui.setPos(x,y);
+    
+    
+    if (xDir == 0){
+    	if (x < 280)
+    	{
+    		++x;
+    	}
+    	else
+    	{
+    		--x;
+    		xDir=1;
+    	}
+    }
+    else
+    {
+    	if (x > 10)
+    	{
+    		--x;
+    	}
+    	else
+    	{
+    		x=x+2;
+    		xDir=0;
+    	}
+    }
+    
+    if (yDir == 0){
+    	if (y < 130)
+    	{
+    		++y;
+    	}
+    	else
+    	{
+    		--y;
+    		yDir=1;
+    	}
+    }
+    else
+    {
+    	if (y > 10)
+    	{
+    		--y;
+    	}
+    	else
+    	{
+    		++y;
+    		yDir=0;
+    	}
+    }  
+    
+    
+    
+
+
+  
+  
+  }
+}
---
+++ src/TimerTaskDemo.java
@@ -0,0 +1,24 @@
+
+import java.util.Timer;
+
+
+public class TimerTaskDemo
+{
+	
+	public static void main( String[] args )
+  {
+
+		gui mygui = new gui();
+			
+		Timer timer = new Timer();
+    
+    // Start in 2 Sekunden
+    //timer.schedule( new Task(),200 );
+
+    // Start in einer Sekunde dann Ablauf alle 5 Sekunden
+		timer.schedule( new Task(mygui), 1000, 10 );
+  }
+  
+  
+  
+}
